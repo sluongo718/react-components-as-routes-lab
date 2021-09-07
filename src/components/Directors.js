e425c247a7129ab73b8b7ee7cff330db4791b215
sluongo718 <sluongo718@yahoo.com>
2021-09-07 02:35:25 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,17 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(dir => {
+        return (
+        <div><h1>{dir.name}</h1>
+        <ul>{dir.movies.map(movie => {
+            return <li>{movie}</li>
+          })}</ul>
+          
+        </div>  
+        )
+      } )}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { move } from 'superagent';
 import { movies } from '../data';
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div> 
+             <h2>Title: {movie.title}</h2>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+                {movie.genres.map(genre => {
+                  return <li>{genre}</li>
+                })}
+              </ul>
+             
+          </div>  
+      )
+        
+      })}
     </div>
   );
 };
